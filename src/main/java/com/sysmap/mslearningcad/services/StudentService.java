45d5d828a781f128f40bafe1e147ebefe5ab5a05
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-17 17:08:52 -0300

    Created createStudent method on StudentService

--- src/main/java/com/sysmap/mslearningcad/services/StudentService.java
+++ src/main/java/com/sysmap/mslearningcad/services/StudentService.java
@@ -1,7 +1,38 @@
 package com.sysmap.mslearningcad.services;
 
+import com.sysmap.mslearningcad.Repositories.StudentRepository;
+import com.sysmap.mslearningcad.controllers.models.CreateStudentInput;
+import com.sysmap.mslearningcad.entities.Student;
+import com.sysmap.mslearningcad.services.models.CreateStudentResponse;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentService(
+        StudentRepository studentRepository
+    ) {
+        this.studentRepository = studentRepository;
+    }
+
+    public CreateStudentResponse createStudent(
+        CreateStudentInput studentInput
+    ) {
+        Student student = new Student();
+        student.setStudentId(UUID.randomUUID());
+        student.setCreatedOn(LocalDateTime.now());
+
+        BeanUtils.copyProperties(studentInput,student);
+
+        var dbResponse = this.studentRepository.insert(student);
+
+        return new CreateStudentResponse(dbResponse.getStudentId());
+    }
 }
