27258d1bafb2cfa5902c4990fe4ecee446dc6ff8
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-17 19:55:51 -0300

    Created get student endpoint

--- src/main/java/com/sysmap/mslearningcad/controllers/StudentController.java
+++ src/main/java/com/sysmap/mslearningcad/controllers/StudentController.java
@@ -4,12 +4,13 @@ import com.sysmap.mslearningcad.controllers.models.CreateStudentInput;
 import com.sysmap.mslearningcad.services.CourseAPIService;
 import com.sysmap.mslearningcad.services.StudentService;
 import com.sysmap.mslearningcad.services.models.CreateStudentResponse;
+import com.sysmap.mslearningcad.services.models.GetStudentResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/v1/student")
@@ -36,5 +37,14 @@ public class StudentController {
         }
         var serviceResponse = this.studentService.createStudent(studentInput);
         return new ResponseEntity<>(serviceResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{studentId}")
+    public ResponseEntity<GetStudentResponse> getStudent(
+       @PathVariable
+       UUID studentId
+    ) {
+        var response = this.studentService.getStudentById(studentId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
