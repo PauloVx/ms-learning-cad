368e8a0b6f6b822b253d28ebb7a82374c2045813
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-17 17:08:31 -0300

    Added all args constructor to CreateStudentResponse

--- src/main/java/com/sysmap/mslearningcad/services/models/CreateStudentResponse.java
+++ src/main/java/com/sysmap/mslearningcad/services/models/CreateStudentResponse.java
@@ -1,10 +1,12 @@
 package com.sysmap.mslearningcad.services.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
 public class CreateStudentResponse {
     private UUID studentId;
 }
