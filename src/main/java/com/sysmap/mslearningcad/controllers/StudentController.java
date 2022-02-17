f464fa15d3f6fb1a2d5a060b7771d6a21c1359e0
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-17 17:43:45 -0300

    Created endpoint to create student

--- src/main/java/com/sysmap/mslearningcad/controllers/StudentController.java
+++ src/main/java/com/sysmap/mslearningcad/controllers/StudentController.java
@@ -1,9 +1,35 @@
 package com.sysmap.mslearningcad.controllers;
 
+import com.sysmap.mslearningcad.controllers.models.CreateStudentInput;
+import com.sysmap.mslearningcad.services.StudentService;
+import com.sysmap.mslearningcad.services.models.CreateStudentResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/v1/student")
 public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(
+        StudentService studentService
+    ) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateStudentResponse> createStudent(
+        @RequestBody
+        CreateStudentInput studentInput
+    ) {
+        var serviceResponse = this.studentService.createStudent(studentInput);
+        return new ResponseEntity<>(serviceResponse, HttpStatus.CREATED);
+    }
 }
