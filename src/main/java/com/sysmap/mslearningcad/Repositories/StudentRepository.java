7cfea93afb2102e574ddc48eb4eaa561cddd8dc5
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-17 16:48:12 -0300

    Create StudentRepository.java

---
+++ src/main/java/com/sysmap/mslearningcad/Repositories/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.sysmap.mslearningcad.Repositories;
+
+import com.sysmap.mslearningcad.entities.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends MongoRepository<Student, UUID> {
+}
