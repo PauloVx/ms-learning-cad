b6c9306ce00fcf78302c040dff455a9c355312b1
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-17 18:44:30 -0300

    Removed unused imports

--- src/main/java/com/sysmap/mslearningcad/services/StudentService.java
+++ src/main/java/com/sysmap/mslearningcad/services/StudentService.java
@@ -5,7 +5,6 @@ import com.sysmap.mslearningcad.controllers.models.CreateStudentInput;
 import com.sysmap.mslearningcad.entities.Student;
 import com.sysmap.mslearningcad.services.models.CreateStudentResponse;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
